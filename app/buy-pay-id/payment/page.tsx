0ddb3aaf947713b3cf635a083c8a0c083d886497
08029686989 <kingkhalifah001@gmail.com>
2025-05-15 15:30:08 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -87,7 +87,7 @@ export default function PayIdPaymentPage() {
             <div className="mb-4">
               <p className="text-gray-600">Account Number</p>
               <div className="flex justify-between items-center">
-                <p className="font-bold">1703014712</p>
+                <p className="font-bold">1100465587</p>
                 <button
                   onClick={() => copyToClipboard("1100465587", "account")}
                   className="bg-orange-400 text-white px-2 py-1 rounded text-sm"
